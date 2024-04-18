47a0b93127c9f90bc2d9d23b895e046ffd383dbe
Ulad <selatnick@gmail.com>
2024-04-18 21:55:54 +0100

    Add .clang-format config and reformat file

--- 01-sum-arrays/main.cu
+++ 01-sum-arrays/main.cu
@@ -4,38 +4,38 @@
 Sum two float arrays
 */
 __global__ void add(float *x, float *y, float *output) {
-  int index = blockIdx.x;
-  output[index] = x[index] + y[index];
+    int index = blockIdx.x;
+    output[index] = x[index] + y[index];
 }
 
 void printArray(float *array, int size) {
-  for (int i = 0; i < size; ++i) {
-    printf("%*.1f ", 4, array[i]);
-  }
-  puts("");
+    for (int i = 0; i < size; ++i) {
+        printf("%*.1f ", 4, array[i]);
+    }
+    puts("");
 }
 
 int main() {
-  int n = 10;
-  float *input_x, *input_y, *output;
-  cudaMallocManaged(&input_x, n * sizeof(float));
-  cudaMallocManaged(&input_y, n * sizeof(float));
-  cudaMallocManaged(&output, n * sizeof(float));
+    int n = 10;
+    float *input_x, *input_y, *output;
+    cudaMallocManaged(&input_x, n * sizeof(float));
+    cudaMallocManaged(&input_y, n * sizeof(float));
+    cudaMallocManaged(&output, n * sizeof(float));
 
-  for (int i = 0; i < n; ++i) {
-    input_x[i] = i;
-    input_y[i] = i * i;
-  }
+    for (int i = 0; i < n; ++i) {
+        input_x[i] = i;
+        input_y[i] = i * i;
+    }
 
-  add<<<n, 1>>>(input_x, input_y, output);
+    add<<<n, 1>>>(input_x, input_y, output);
 
-  cudaDeviceSynchronize();
+    cudaDeviceSynchronize();
 
-  printArray(input_x, n);
-  puts("+");
-  printArray(input_y, n);
-  puts("=");
-  printArray(output, n);
+    printArray(input_x, n);
+    puts("+");
+    printArray(input_y, n);
+    puts("=");
+    printArray(output, n);
 
-  return 0;
+    return 0;
 }
