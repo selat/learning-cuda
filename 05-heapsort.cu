8fb496b9f0b8e8bc4331aee84ff29a9a8e00e3ba
Ulad <selatnick@gmail.com>
2024-04-22 18:11:59 +0100

    GPU merging for heap sort

--- 05-heapsort.cu
+++ 05-heapsort.cu
@@ -5,10 +5,10 @@
 /*
 Sort a vector using heap sort
 
-Current timings for n=50_000_000:
-qsort CPU sorted in 3284.454ms
-Heap CPU sorted in 5791.861ms
-GPU sorted in 1758.085ms, GPU time 716.513ms
+Current timings for n=51_200_000:
+qsort CPU sorted in 4501.101ms
+Heap CPU sorted in 18597.955ms
+GPU sorted in 1987.191ms, GPU time 70.268ms
 */
 
 #define THREADS_PER_BLOCK 1024
@@ -141,6 +141,36 @@ __global__ void kernelBitonicSort(int *vector, int n) {
     vector[idx] = local_vector[tid];
 }
 
+__global__ void kernelMerge(int* vector, int *output_vector, int n, int block_size) {
+    unsigned int idx = blockIdx.x * block_size * 2;
+
+    int left = idx;
+    int right = idx + block_size;
+    int left_end = right;
+    int right_end = right + block_size;
+    int i = 0;
+    while (left < left_end && right < right_end) {
+        if (vector[left] < vector[right]) {
+            output_vector[idx + i] = vector[left];
+            ++left;
+        } else {
+            output_vector[idx + i] = vector[right];
+            ++right;
+        }
+        ++i;
+    }
+    while (left < left_end) {
+        output_vector[idx + i] = vector[left];
+        ++left;
+        ++i;
+    }
+    while (right < right_end) {
+        output_vector[idx + i] = vector[right];
+        ++right;
+        ++i;
+    }
+}
+
 void heapSort(int *vector, int n) {
     PriorityQueue queue;
     init(&queue, n);
@@ -180,7 +210,9 @@ int main() {
     }
 
     int *gpu_vector;
+    int *gpu_tmp_vector;
     cudaMallocManaged(&gpu_vector, n * sizeof(int));
+    cudaMallocManaged(&gpu_tmp_vector, n * sizeof(int));
     for (int i = 0; i < n; i++) {
         gpu_vector[i] = vector[i];
     }
@@ -214,16 +246,22 @@ int main() {
     const int blocks_num = (n + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;
     // kernelBubbleSort<<<blocks_num, THREADS_PER_BLOCK>>>(gpu_vector, n);
     kernelBitonicSort<<<blocks_num, THREADS_PER_BLOCK>>>(gpu_vector, n);
+    kernelMerge<<<blocks_num / 2, 1>>>(gpu_vector, gpu_tmp_vector, n, THREADS_PER_BLOCK);
+    cudaMemcpy(gpu_vector, gpu_tmp_vector, n * sizeof(int), cudaMemcpyDeviceToDevice);
+    kernelMerge<<<blocks_num / 4, 1>>>(gpu_vector, gpu_tmp_vector, n, THREADS_PER_BLOCK * 2);
+    cudaMemcpy(gpu_vector, gpu_tmp_vector, n * sizeof(int), cudaMemcpyDeviceToDevice);
+    kernelMerge<<<blocks_num / 8, 1>>>(gpu_vector, gpu_tmp_vector, n, THREADS_PER_BLOCK * 4);
+    cudaMemcpy(gpu_vector, gpu_tmp_vector, n * sizeof(int), cudaMemcpyDeviceToDevice);
+    kernelMerge<<<blocks_num / 16, 1>>>(gpu_vector, gpu_tmp_vector, n, THREADS_PER_BLOCK * 8);
+    cudaMemcpy(gpu_vector, gpu_tmp_vector, n * sizeof(int), cudaMemcpyDeviceToDevice);
     cudaEventRecord(cuda_end);
     cudaError_t error = cudaDeviceSynchronize();
     if (error != cudaSuccess) {
         fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(error));
     }
 
-    printf("Blocks num %d\n", blocks_num);
-
     int *gpu_output_vector = (int *)malloc(n * sizeof(int));
-    mergeOutputVector(gpu_vector, gpu_output_vector, n, THREADS_PER_BLOCK, blocks_num);
+    mergeOutputVector(gpu_vector, gpu_output_vector, n, THREADS_PER_BLOCK * 16, blocks_num / 16);
     gettimeofday(&end, NULL);
 
     for (int i = 0; i < n; ++i) {
